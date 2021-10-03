52bbff723769fb0cd17a45c3677cdca6c2069573
Oscar Gonzalez <imoskgn@gmail.com>
2021-10-03 15:37:06 -0400

    Services Content Wrap up

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,3 +1,10 @@
+/*  File Name: app.js
+    Student: Oscar Eduardo Gonzalez-Neira 
+    Code: 301185878
+    ---------------------------------------------
+    Date            Update
+    03/10/2021  Initial implementation
+*/
 // IIFE Immediately Invoked Function Expression
 (function(){
     function Start(){
